385a833e577e19bc47d500224c3a546a368e6ebb
sahithichirla <sahithichirla@gmail.com>
2024-02-14 16:24:07 +0530

    added a new project

---
+++ XamarinBinding/XamarinBinding/XamarinBinding/Page1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinBinding
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            Content = new Label
+            {
+                Text = "Hello now render via code behind...",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+        }
+    }
+}
