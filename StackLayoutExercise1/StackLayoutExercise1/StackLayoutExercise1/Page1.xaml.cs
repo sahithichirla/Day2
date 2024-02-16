1a02a3cde134c3138510f7fbbd0eef5f82c6adf6
sahithichirla <sahithichirla@gmail.com>
2024-02-16 13:14:40 +0530

    add a comment

---
+++ StackLayoutExercise1/StackLayoutExercise1/StackLayoutExercise1/Page1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StackLayoutExercise1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Login(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Register(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
